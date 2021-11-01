56d2aced964a7c923cbe7993e0afcdd53224f06e
perkles <anderson.os88@hotmail.com>
2021-11-01 10:53:14 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
-import './App.css';
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
 function App() {
   return (
-    <div>
-      <h1 className="test-primary">olá mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">olá mundo!</h1>
+        <DataTable />
+      </div>
+      <Footer/>
+
+
+    </>
   );
 }
 
