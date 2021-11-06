c1abec66b3f73173e8179a33ff12c11c6ec59bfc
perkles <anderson.os88@hotmail.com>
2021-11-06 09:30:13 -0300

    update v1

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 const Pagination = ({ page, onPageChange } : Props) => {
     return (
-        <div className="row d-flex justify-content-center">
+        <div className="d-flex justify-content-center mx auto">
             <nav>
                 <ul className="pagination">
                 <li className={`page-item ${page.first ? 'disabled' : ''} `}>
